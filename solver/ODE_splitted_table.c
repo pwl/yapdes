4df096369f884728dba97fddb9ef58627e715fd3
Paweł Biernat <pawel.biernat@gmail.com>
2010-01-21 17:26:27 +0100

    Added accessors to ODE_splitted_table

--- solver/ODE_splitted_table.c
+++ solver/ODE_splitted_table.c
@@ -2,27 +2,57 @@
 
 ODE_splitted_table * ODE_splitted_table_init( int N, int * length )
 {
-  int i;
-  ODE_splitted_table * st =
-    malloc( sizeof( ODE_splitted_table ) );
+  int i,total_length=0;
+  ODE_splitted_table * st=malloc( sizeof( ODE_splitted_table ) );
 
   for( i = 0; i < N; i++)
-    {
-      st->table[i]=malloc( length[i] * sizeof( ODE_R ) );
-      st->length[i]=length[i];
-    }
+    total_length+=length[i];
 
+  st->length=length;
+  st->table = malloc( total_length*sizeof( ODE_R ) );
   return st;
 }
 
 void ODE_splitted_table_free( ODE_splitted_table * st )
 {
-  int i;
-  for( i = 0; i < st->N; i++)
-    {
-      free( st->table[i] );
-    }
-
   free( st->length );
+  free( st->table );
   free( st );
+}
+
+int ODE_splitted_table_get_table( ODE_splitted_table * st, int n, ODE_table * t )
+{
+  if( n < st->N )
+    {
+      t->table = st->table+n;
+      t->length = st->length[n];
+      return 0;
+    }
+  else				/* no enough elements int st*/
+    return -1;
+}
+
+int ODE_splitted_table_to_table( ODE_splitted_table * st, int n, ODE_table * t )
+{
+  if( n >= st->N )
+    return -1;			/* not enugh elements in st */
+
+  t->length = st->length[n];
+  t->table = ODE_splitted_table_to_ptr( st, n );
+
+  return 0;
+}
+
+
+ODE_R * ODE_splitted_table_to_ptr( ODE_splitted_table * st, int n )
+{
+  ODE_R * pt = st->table;
+
+  if( n >= st->N )
+    return NULL;			/* not enugh elements in st */
+
+  for ( n = 0; n > 0; n-- )
+    pt+=st->length[n];		/* calculate the position of the right
+				   table */
+  return pt;
 }
--- solver/ODE_splitted_table.h
+++ solver/ODE_splitted_table.h
@@ -1,6 +1,7 @@
 #ifndef _ODE_SPLITTED_TABLE_H_
 #define _ODE_SPLITTED_TABLE_H_
 
+#include "ODE_table.h"
 #include "ODE_common.h"
 
 
@@ -10,30 +11,49 @@ struct _ODE_splitted_table
 {
   int N;			/* number of tables */
   int * length;			/* length of the particular table */
-  ODE_R ** table;		/* tables */
+  ODE_R * table;		/* table to by sliced */
 };
 
 /**
- * @file   ODE_splitted_table.h
- * @author Pawel Biernat <pawel.biernat@gmail.com>
- * @date   Wed Jan 20 13:10:20 2010
  *
- * @brief The function below initializes the basic data structure for
- * the ODE_solver
+ *
+ * @param N number of elements in length
+ * @param length length of slices
+ *
+ * @return
  */
 
 ODE_splitted_table * ODE_splitted_table_init (int N, int * length);
 
 /**
- * @file   ODE_splitted_table.h
- * @author Pawel Biernat <pawel.biernat@gmail.com>
- * @date   Wed Jan 20 13:20:06 2010
  *
- * @brief The functio below frees the memory used by type
- * ODE_splitted_table
+ *
+ * @param st
  */
 
 void ODE_splitted_table_free( ODE_splitted_table * st );
 
+/**
+ * This function fills t with n-th data set from st
+ *
+ * @param st
+ * @param n
+ *
+ * @return
+ */
+
+int ODE_splitted_table_get( ODE_splitted_table * st, int n, ODE_table * t );
+
+/**
+ * This function returns the pointer to the n-th array from
+ * ODE_splitted_table
+ *
+ * @param st
+ * @param n
+ *
+ * @return
+ */
+
+ODE_R * ODE_splitted_table_to_ptr( ODE_splitted_table * st, int n );
 
 #endif /* _ODE_SPLITTED_TABLE_H_ */

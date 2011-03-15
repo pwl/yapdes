5b41f2e9c01a9549331f2061002d6f7c3cac2c71
Paweł Biernat <pawel.biernat@gmail.com>
2011-03-15 01:21:40 +0100

    Added a doxygen front page file

---
+++ src/frontpage.h
@@ -0,0 +1,9 @@
+/**
+   \mainpage The frontpage of yapdes
+   \section s First section
+   text
+   \subsection ss Subsection: LaTeX test
+   \f{equation}{
+   \sum_{n=1}^\infty n=-\frac{1}{12}
+   \f}
+ */

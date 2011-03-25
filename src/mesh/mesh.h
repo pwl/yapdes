204c8aa93334b49335ccc1220e4a5d1ce7b8027e
Paweł Biernat <pawel.biernat@gmail.com>
2011-03-25 10:17:32 +0100

    Mesh?

---
+++ src/mesh/mesh.h
@@ -0,0 +1,17 @@
+/**
+ * @file   mesh.h
+ * @author Pawel Biernat <pawel.biernat@gmail.com>
+ * @date   Thu Mar 24 17:06:09 2011
+ *
+ * @brief A prototype of a mesh structure to store a set of points
+ * along with a numerical differentiation, interpolation etc.
+ *
+ *
+ */
+
+#ifndef _MESH_H_
+#define _MESH_H_
+
+
+
+#endif /* _MESH_H_ */
